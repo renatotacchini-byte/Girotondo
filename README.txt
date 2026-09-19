e72e3cd3132cc88955b01f466652f4ae899f4430
renatotacchini-byte <renatotacchini@gmail.com>
2026-09-19 18:34:28 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-# Girotondo 0.4 PWA
-Prototipo mobile installabile come web-app. Dati di prova: giri 1040, 1042, 1043.
-Per pubblicarlo: caricare questi tre file su un hosting HTTPS statico.
+Girotondo 0.5 - PWA prototype
+Versione con icone installabili e manifest PWA completo.
+Dati presenti esclusivamente di prova: non inserire dati reali di minori su GitHub Pages pubblico.
--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
-const CACHE='girotondo-0.4';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE='girotondo-0.5';
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icons/icon-192.png',
+      './icons/icon-512.png'
+    ]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
